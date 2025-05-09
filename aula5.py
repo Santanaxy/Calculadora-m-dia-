3fe4e627351afdcde39f93d56f3dbb4e48548975
Santanaxy <victorsantana.rezende@gmail.com>
2025-05-09 15:53:04 -0300

    Add files via upload

---
+++ aula5.py
@@ -0,0 +1,52 @@
+média = input("digite nome do aluno: ")  #este codigo calcula a média do 1° aluno 
+
+nota1 = float(input("digite a primeira nota: " )) #escrever a primeira nota do aluno em numero real 
+nota2= float(input("digite a segunda nota: "))#escrever a segunda nota do aluno em numero real 
+nota3 = float(input("digite a terceira nota: ")) #escrever a terceira nota do aluno em numero real 
+
+média = (nota1 + nota2 + nota3) / 3   # média e a soma das notas divido por 3 
+
+print(f"A média do aluno é:  {média: .2f} ") # mostra o resultado da média com duas casas após a virgula, por isso tem que colocar {média: .2f} para andar duas casas antes a viruglas 
+
+if média >=7:
+    print("APROVADO")
+
+else:
+    print("REPROVADO :(")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#adição + 
+#subtração - 
+#divisão / 
+# multiplicação *
+# maior igual >= 
+#menor igual <=
+
+#if= SE 
+#else senão
